035ae17db5b72466249acc7fe6be162dc38ba65c
anantp1910 <anant.p1910@gmail.com>
2026-01-17 18:48:07 -0500

    Update

--- app/src/main/java/com/example/watchcareai/MainActivity.kt
+++ app/src/main/java/com/example/watchcareai/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.watchcareai
+import com.google.firebase.firestore.FirebaseFirestore
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,10 +8,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        db.collection("test")
+            .add(mapOf("hello" to "world"))
+            .addOnSuccessListener { println("Firestore write success!") }
+            .addOnFailureListener { e -> e.printStackTrace() }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
